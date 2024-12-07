b1917b548ceb919ba28477ca4cdfa6efed9214a6
hrn-tmy <mj0829mj0625@icloud.com>
2024-12-07 20:43:03 +0900

    クライアントストリーミングのスキーマ定義ならびにサーバーとクライアントの実装

--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"go-grpc/pb"
@@ -68,6 +69,25 @@ func (*server) Download(req *pb.DownloadRequest, stream pb.FileService_DownloadS
 		time.Sleep(1 * time.Second)
 	}
 	return nil
+}
+
+func (*server) Upload(stream pb.FileService_UploadServer) error {
+	fmt.Println("Upload was invoked")
+	var buf bytes.Buffer
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			res := &pb.UploadResponse{Size: int32(buf.Len())}
+			return stream.SendAndClose(res)
+		}
+		if err != nil {
+			return err
+		}
+		data := req.GetData()
+		log.Printf("received data(bytes): %v", data)
+		log.Printf("received data(bytes): %v", string(data))
+		buf.Write(data)
+	}
 }
 
 func main() {
